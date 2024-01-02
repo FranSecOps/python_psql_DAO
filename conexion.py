43f9c365e719461fb4255ae7af9e52a9e4422923
Francisco Mancilla <137026718+FranSecOps@users.noreply.github.com>
2024-01-01 20:20:46 -0400

    Update conexion.py

--- conexion.py
+++ conexion.py
@@ -5,7 +5,7 @@ import sys
 class Conexion:
     _DATABASE = "test_db"
     _USERNAME = "postgres"
-    _PASSWORD = "L8UVKkmQG1"
+    _PASSWORD = "password"
     _DB_PORT = "5432"
     _HOST = "127.0.0.1"
     _MIN_CON = 1
